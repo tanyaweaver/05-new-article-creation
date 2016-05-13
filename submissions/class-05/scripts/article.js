a1ddb842325508221e30df909a9f63470bc70416
tanyaweaver <tanya83@uw.edu>
2016-05-13 15:43:08 -0700

    added functionality from the workshop

---
+++ submissions/class-05/scripts/article.js
@@ -0,0 +1,25 @@
+var articles = [];
+
+function Article (opts) {
+  for (keys in opts) {
+    this[keys] = opts[keys];
+  };
+}
+
+Article.prototype.toHtml = function(scriptTemplateId) {
+  var template = Handlebars.compile($(scriptTemplateId).html());
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  this.body = marked(this.body);
+
+  return template(this);
+};
+
+ourLocalData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+ourLocalData.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
